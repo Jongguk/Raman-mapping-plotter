5041f30b9155b9dcf5445c868a8753f3bb108df5
Jongguk <jonggukahn@gmail.com>
2024-03-19 19:57:41 +0900

    Subtract constant baseline from raw data

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def export_mapping(output_folder = output):
         filetypes=[("CSV files", "*.csv"), ("All files", "*.*")]
     )
     if file_path:
-        export_2d_data(integrated_area, file_path)
+        export_2d_data(integrated_area_baseline_subtract, file_path)
         print(f"Integrated area data saved to: {file_path}")
 
 # Function to handle export button click for spectrum
@@ -87,12 +87,30 @@ selection_mask = select_area(selected_area_begin, selected_area_end)
 line_number = int(plot_x * PIXEL_COUNT + plot_y)
 spectrum_intensity = result[line_number]
 
+def find_index_baseline(arr = spectrum_energy, selected_area_begin : int = selected_area_begin, selected_area_end : int = selected_area_end):
+    baseline_left_index = None
+    baseline_right_index = None
+    for j, num in enumerate(spectrum_energy):
+        if num <= selected_area_begin:
+            if baseline_left_index is None or spectrum_energy[j] > spectrum_energy[baseline_left_index]:
+                baseline_left_index = j
+    for i, num in enumerate(spectrum_energy):
+        if num >= selected_area_end:
+            if baseline_right_index is None or spectrum_energy[i] < spectrum_energy[baseline_right_index]:
+                baseline_right_index = i
+    return baseline_left_index, baseline_right_index
+
 # Calculate total area from the selected energy range
+selected_point_count = selection_mask.sum()
 integrated_area = np.zeros((PIXEL_COUNT, PIXEL_COUNT))
+integrated_area_baseline_subtract = np.zeros((PIXEL_COUNT, PIXEL_COUNT))
 i = 0
 for y in range(PIXEL_COUNT):
     for x in range(PIXEL_COUNT):
         integrated_area[x, y] = (result[i + 1] * selection_mask).sum()
+        baseline_left = result[i + 1][find_index_baseline(result[i + 1])[0]]
+        baseline_right = result[i + 1][find_index_baseline(result[i + 1])[1]]
+        integrated_area_baseline_subtract[x, y] = integrated_area[x, y] - (selected_point_count * (baseline_left + baseline_right) * 0.5)
         i += 1
 
 # Accumulate the count in the file name
@@ -113,7 +131,7 @@ with open(output_file_path, 'w') as file:  # Modified to use the calculated outp
 # Plot the rotated image with adjusted intensity range
 fig, axs = plt.subplots(1, 2, figsize=(10, 4.5), gridspec_kw={'width_ratios': [3, 2]})
 
-im = axs[0].imshow(integrated_area, interpolation='none', aspect='equal')
+im = axs[0].imshow(integrated_area_baseline_subtract, interpolation='none', aspect='equal')
 fig.colorbar(im, ax=axs[0], label='Intensity (a.u)')  # Add color bar indicating intensity
 axs[0].set_xlabel('')  # Label for the x-axis
 axs[0].set_ylabel('')  # Label for the y-axis
